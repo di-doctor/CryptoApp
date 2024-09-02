2fc6b8ede8737cbc367da0e0bc0b78ec3e2549c3
Dima Tambovskiy <di-doctor@yandex.ru>
2024-09-02 23:46:51 +0300

    refactor1

---
+++ app/src/main/java/com/example/cryptoapp/domain/GetCoinInfoUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.cryptoapp.domain
+
+import androidx.lifecycle.LiveData
+
+class GetCoinInfoUseCase(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(fromSymbol: String) = repository.getCoinInfo(fromSymbol = fromSymbol)
+}
+
